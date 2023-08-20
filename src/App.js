cf831c3d2005a856ef9acecb2e2faabb706c6c6e
xanyl <aniltiwari645@gmail.com>
2023-08-20 09:56:00 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
-
+                        <div> Hellow Wprof</div>
                         {/* Add Remaining component here under */}        
 
                         {/* Add ExpenseTotal component here under */}        
