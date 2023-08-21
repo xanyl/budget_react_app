2e166b248e0d868aa15e3ca76278e82f1280511a
xanyl <aniltiwari645@gmail.com>
2023-08-21 09:54:45 -0400

    Adding input in budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,35 @@
 
-import React, { useContext } from 'react';
+import React, {useState, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const [dispatch, Currency] = useContext(AppContext);
+    const [budget, setBudget] = useState("");
+
+    const handleBudgetChange =(event) => {
+        console.log("new budget", event.target.value);
+        setBudget(parseInt(event.target.value));
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.keyCode === 13){
+            console.log("enter key pressed");
+            dispatch({type:"SET_BUDGET", payload: budget});
+        }
+    }
+
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+        <div className="alert alert-secondary">
+            <label htmlFor="cost">Budget: {Currency}</label>
+            <input
+                required
+                type="number"
+                id="cost"
+                value={budget}
+                style={{ marginLeft: "2rem", width: "10rem" }}
+                onChange={handleBudgetChange}
+                onKeyDown = {handleKeyDown}
+               
+            />
         </div>
     );
 };
